f223c399ed2f5504773442fee2d0809fda5bd823
Freddy Mejia <100593377+Freddy1087@users.noreply.github.com>
2022-05-04 23:08:26 -0400

    Add files via upload

---
+++ Notes on Website Development.js
@@ -0,0 +1,22 @@
+/*
+
+3 Essential Parts to Website Development
+
+1) HTML (HyperText Markup Language)
+>> Adds structure to the web pages
+>> Tags ued for e.g. <div>, <section>, <p>
+
+2) CSS (Cascading Style Sheets)
+>> Adds styles to our webpages.
+>> E.G. colors, border, margin, image, positions, etc.
+>> Can use IDs, classes, or direct tags to reference HTML tags
+
+3) JS (JavaScript)
+>> Adds programming to our web pagtes.
+>> Adds functionality e.g. client side validation effects & events etc.
+
+
+
+
+
+*/
